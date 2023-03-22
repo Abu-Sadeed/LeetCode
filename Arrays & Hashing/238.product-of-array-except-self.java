8f22f824629049f89282c7f7c71e993c614ff874
Sadeed <abusadeed@iut-dhaka.edu>
2023-03-22 22:35:16 +0600

    tried reducing space comp of problem 238

--- Arrays & Hashing/238.product-of-array-except-self.java
+++ Arrays & Hashing/238.product-of-array-except-self.java
@@ -8,25 +8,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int lens = nums.length;
-
-        int[] left = new int[lens];
-        int[] right = new int[lens];
         int[] result = new int[lens];
 
-        left[0] = 1;
-        right[lens - 1] = 1;
+        result[0] = 1;
+
+        int r = 1;
 
         for (int i = 1; i < lens; i++) {
-            left[i] = nums[i - 1] * left[i - 1];
+            result[i] = nums[i - 1] * result[i - 1];
 
         }
 
-        for (int i = lens - 2; i >= 0; i--) {
-            right[i] = nums[i + 1] * right[i + 1];
-        }
+        for (int i = lens - 1; i >= 0; i--) {
+            result[i] = result[i] * r;
 
-        for (int i = 0; i < lens; i++) {
-            result[i] = left[i] * right[i];
+            r = r * nums[i];
         }
 
         return result;
