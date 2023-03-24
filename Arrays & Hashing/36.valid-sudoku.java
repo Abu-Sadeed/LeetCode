f8aa1cc1adfd20df2ce45602dcd11589640644ed
Sadeed <abusadeed@iut-dhaka.edu>
2023-03-24 23:07:23 +0600

    solved 36 valid sudoku in java

---
+++ Arrays & Hashing/36.valid-sudoku.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+/*
+ * @lc app=leetcode id=36 lang=java
+ *
+ * [36] Valid Sudoku
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<String>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char current = board[i][j];
+                if (current != '.' && (!seen.add(current + " found in row " + i) ||
+                        !seen.add(current + " found in column " + j) ||
+                        !seen.add(current + " found in sub box " + i / 3 + "-" + j / 3))) {
+                    return false;
+
+                }
+
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
